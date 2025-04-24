2a38f669a80f081f6a7bb224df76ca3becd89285
masozow <14838605+masozow@users.noreply.github.com>
2025-04-24 10:25:20 -0600

    creating the accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,5 +1,40 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "Is it styled?",
+    answer:
+      "Yes. It comes with default styles that matches the other components' aesthetic.",
+  },
+  {
+    id: "item-3",
+    question: "Is it animated?",
+    answer:
+      "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+];
 const Page = () => {
-  return <div>Accordion Page</div>;
+  return (
+    <Accordion type="single" className="w-full">
+      {items.map((item) => (
+        <AccordionItem value={item.id} key={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
 };
 
 export default Page;
