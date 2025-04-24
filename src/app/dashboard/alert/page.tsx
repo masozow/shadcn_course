e8a0d153837377cf0753357743a4cb2ac5f7aa14
masozow <14838605+masozow@users.noreply.github.com>
2025-04-24 11:51:15 -0600

    adding the alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,5 +1,32 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
 const Page = () => {
-  return <div>Alert Page</div>;
+  return (
+    <div className="grid gap-3">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 };
 
 export default Page;
