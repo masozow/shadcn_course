cfee50c6362bb33850adedcf0409baafe7c8a843
masozow <14838605+masozow@users.noreply.github.com>
2025-04-28 12:53:08 -0600

    adding styles to the avatar and a text

--- src/app/dashboard/avatar/page.tsx
+++ src/app/dashboard/avatar/page.tsx
@@ -1,11 +1,13 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const Page = () => {
   return (
-    <div>
+    <div className="h-[500px] flex justify-center items-center">
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
+
+      <p className="m-2">@shadcn</p>
     </div>
   );
 };
