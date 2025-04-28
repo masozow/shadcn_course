96d974f041edc747b3ae1524e69ab5f5630444a4
masozow <14838605+masozow@users.noreply.github.com>
2025-04-28 10:18:32 -0600

    adding a partial programmatic control over the dialog using state

--- src/app/dashboard/alert-dialog/page.tsx
+++ src/app/dashboard/alert-dialog/page.tsx
@@ -11,31 +11,38 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const Page = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
   return (
-    <AlertDialog onOpenChange={(value)=>console.log({value})}>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Show Dialog</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={() => console.log("Cancel")}>
-            Cancel
-          </AlertDialogCancel>
-          <AlertDialogAction onClick={() => console.log("Continue")}>
-            Continue
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <>
+      <AlertDialog
+        open={dialogOpen}
+        onOpenChange={(value) => console.log({ value })}>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => console.log("Cancel")}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={() => console.log("Continue")}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Button onClick={() => setDialogOpen(true)}>Open dialog</Button>
+    </>
   );
 };
 
