ba314e3894f1a650bb2e8ea939e8517b49ca93a6
masozow <14838605+masozow@users.noreply.github.com>
2025-04-28 14:33:05 -0600

    adding a card with an input and a label, making it responsive

--- src/app/dashboard/card/page.tsx
+++ src/app/dashboard/card/page.tsx
@@ -7,7 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 const Page = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
@@ -26,6 +27,28 @@ const Page = () => {
           </CardFooter>
         </Card>
       ))}
+      <Card className="col-span-1 sm:col-span-3">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
