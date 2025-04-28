5be4dfc125ad326687da2680c172de0b1f916dfa
masozow <14838605+masozow@users.noreply.github.com>
2025-04-28 12:51:05 -0600

    adding the avatar component on it's corresponding page

---
+++ src/app/dashboard/avatar/page.tsx
@@ -0,0 +1,13 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+const Page = () => {
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
+
+export default Page;
