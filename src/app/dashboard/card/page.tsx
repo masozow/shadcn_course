c5aaedcba603a0132b820eef4ebb712655db8b24
masozow <14838605+masozow@users.noreply.github.com>
2025-04-28 13:00:10 -0600

    adding the card component on it's page

---
+++ src/app/dashboard/card/page.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const Page = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Page;
