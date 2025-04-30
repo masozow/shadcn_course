fd0ac89c2e79d914e37c5516c7f7657fe290185f
masozow <14838605+masozow@users.noreply.github.com>
2025-04-30 16:23:39 -0600

    adding the keyboard command functionality

--- src/app/dashboard/command/page.tsx
+++ src/app/dashboard/command/page.tsx
@@ -19,10 +19,21 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Page = () => {
   const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, []);
   return (
     <div>
       <CommandDialog open={open} onOpenChange={(value) => setOpen(value)}>
@@ -63,6 +74,18 @@ const Page = () => {
           </CommandGroup>
         </CommandList>
       </CommandDialog>
+      <div className="mt-10 h-[200px] flex justify-center items-center">
+        <p className="text-sm text-muted-foreground">
+          Press{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">⌘</span>J
+          </kbd>{" "}
+          or{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">CTRL</span>+ J
+          </kbd>
+        </p>
+      </div>
     </div>
   );
 };
