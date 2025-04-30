ad3190c2332d6fc32100a52ad732828ce4418802
masozow <14838605+masozow@users.noreply.github.com>
2025-04-30 08:20:52 -0600

    adding the carousel to it's corresponding page

--- src/app/dashboard/carousel/page.tsx
+++ src/app/dashboard/carousel/page.tsx
@@ -1,5 +1,28 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 const Page = () => {
-  return <div>Carousel Page</div>;
+  return (
+    <div>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">...</CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">...</CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default Page;
