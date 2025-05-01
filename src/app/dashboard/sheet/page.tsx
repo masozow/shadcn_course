739d66658cb4c3131a6638b9774cd5ced83496fb
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 10:42:15 -0600

    adding 4 buttons to show the sheet on different sides of the page

--- src/app/dashboard/sheet/page.tsx
+++ src/app/dashboard/sheet/page.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/sheet";
 import { useState } from "react";
 
+const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
 const Page = () => {
   const [open, setOpen] = useState(false);
   return (
@@ -27,6 +28,22 @@ const Page = () => {
           </SheetHeader>
         </SheetContent>
       </Sheet>
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger className="border-2 border-dashed rounded">
+            Open {side}
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      ))}
     </div>
   );
 };
