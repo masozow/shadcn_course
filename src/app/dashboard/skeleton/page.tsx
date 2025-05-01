066e9ced16890c9233ff11c05af90f61e1ce628d
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 11:46:32 -0600

    creating 9 cards with a delay in their load

--- src/app/dashboard/skeleton/page.tsx
+++ src/app/dashboard/skeleton/page.tsx
@@ -8,27 +8,35 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 
-const Page = () => {
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return "123456789".split(""); // ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+};
+
+const Page = async () => {
+  const data = await getData();
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-      <Card>
-        <CardHeader>
-          <Image
-            src="https://github.com/shadcn.png"
-            alt="@shadcn"
-            width={40}
-            height={40}
-            className="rounded-full mr-2 w-10 h-10"
-          />
-          <div>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </div>
-        </CardHeader>
-        <CardFooter className="flex justify-end">
-          <Button>More</Button>
-        </CardFooter>
-      </Card>
+      {data.map((item) => (
+        <Card key={item}>
+          <CardHeader className="flex-row">
+            <Image
+              src="https://github.com/shadcn.png"
+              alt="@shadcn"
+              width={40}
+              height={40}
+              className="rounded-full mr-2 w-10 h-10"
+            />
+            <div>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </div>
+          </CardHeader>
+          <CardFooter className="flex justify-end">
+            <Button>More</Button>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 };
