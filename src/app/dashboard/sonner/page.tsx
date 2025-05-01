f6bc5ac09c16ca244c84c586597f59664adffe7e
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 12:46:14 -0600

    adding another toaster with other variant

--- src/app/dashboard/sonner/page.tsx
+++ src/app/dashboard/sonner/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
 const Page = () => {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="grid grid-cols-4 gap-4">
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast("Event has been created", {
+            duration: 5000,
+            position: "top-right",
+            description: `Sunday, December 03, ${new Date().getFullYear()} at 9:00 AM`,
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }>
+        Show Toast
+      </Button>
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast.success("Event has been created", {
+            duration: 5000,
+            position: "top-right",
+            description: `Sunday, December 03, ${new Date().getFullYear()} at 9:00 AM`,
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }>
+        Show Toast
+      </Button>
     </div>
   );
 };
