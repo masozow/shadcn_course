4bc6235edb88a91a1d11166c011926e7e400f45a
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 10:37:03 -0600

    adding the code for the sheet component, and adding an aditional button to open the sheet programmatically

--- src/app/dashboard/sheet/page.tsx
+++ src/app/dashboard/sheet/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+
 const Page = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <h1>Hello Sheet</h1>
+    <div className="grid grid-cols-2 gap-3">
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
