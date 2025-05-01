1b2442a8287e7f0b3818380c729c82bf9705a2d7
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 12:37:13 -0600

    changing the name of the toaster import, to sonnerToaster, to avoid collision with the original toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -37,7 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange>
           {children}
-          <Toaster />
+          <SonnerToaster />
         </ThemeProvider>
       </body>
     </html>
