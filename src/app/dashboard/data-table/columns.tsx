bc9236969b0b9d9b4118be80333fa2aba3d7d85d
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 15:33:50 -0600

    adding formatting to amount column

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -5,19 +5,29 @@ import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Payment>[] = [
   {
+    accessorKey: "clientName",
+    header: "Client Name",
+  },
+  {
     accessorKey: "status",
     header: "Status",
+  },
+
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-  {
-    accessorKey: "clientName",
-    header: "Client Name",
   },
 ];
