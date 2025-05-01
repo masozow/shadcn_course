af91c662b228259d7cba073c9f2c3bc041cf2a7b
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 09:46:05 -0600

    adding functionality and state to the input-top component

--- src/app/dashboard/input-otp/page.tsx
+++ src/app/dashboard/input-otp/page.tsx
@@ -1,26 +1,28 @@
+"use client";
 import {
   InputOTP,
   InputOTPGroup,
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { useState } from "react";
 const Page = () => {
+  const [value, setValue] = useState("");
   return (
-    <div className="flex justify-center items-center h-[250px]">
+    <div className="flex flex-col justify-center items-center h-[250px]">
       {" "}
-      <InputOTP maxLength={6}>
+      <InputOTP
+        maxLength={4}
+        value={value}
+        onChange={(value) => setValue(value)}>
         <InputOTPGroup>
           <InputOTPSlot index={0} />
           <InputOTPSlot index={1} />
           <InputOTPSlot index={2} />
-        </InputOTPGroup>
-        <InputOTPSeparator />
-        <InputOTPGroup>
           <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
         </InputOTPGroup>
       </InputOTP>
+      <div className="mt-4">{value}</div>
     </div>
   );
 };
