cf6926562636d2819d8317e64cf957f603bf2be1
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 10:18:07 -0600

    modyfing the way the progress bar is being filled

--- src/app/dashboard/progress/page.tsx
+++ src/app/dashboard/progress/page.tsx
@@ -4,11 +4,21 @@ import { useEffect, useState } from "react";
 import { Progress } from "@/components/ui/progress";
 
 const Page = () => {
-  const [progress, setProgress] = useState(13);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return prev;
+        }
+        return prev + 1;
+      });
+      return () => {
+        clearInterval(interval);
+      };
+    }, 100);
   }, []);
   return (
     <div>
