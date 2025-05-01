1ff8cd4486f4dec55a0acabc3997269453635c4b
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 11:29:44 -0600

    creating a structure for some generic content, that will be replaced by skeleton while it loads. Added a configuration in next.config.ts to allow the image remote source

--- src/app/dashboard/skeleton/page.tsx
+++ src/app/dashboard/skeleton/page.tsx
@@ -1,7 +1,34 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
 const Page = () => {
   return (
-    <div>
-      <h1>Hello Skeleton</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      <Card>
+        <CardHeader>
+          <Image
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+            width={40}
+            height={40}
+            className="rounded-full mr-2 w-10 h-10"
+          />
+          <div>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <Button>More</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
