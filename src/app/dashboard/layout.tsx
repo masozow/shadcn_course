8321c8200b6e902932a31aeb1675b35022aaab62
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 10:30:18 -0600

    adding the sheet component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -32,6 +32,7 @@ const links = [
   { name: "menu bar", href: "menu-bar" },
   { name: "input OTP", href: "input-otp" },
   { name: "progress", href: "progress" },
+  { name: "sheet", href: "sheet" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
---
+++ src/app/dashboard/sheet/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Hello Sheet</h1>
+    </div>
+  );
+};
+
+export default Page;
