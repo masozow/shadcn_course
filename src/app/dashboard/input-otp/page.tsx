f9655eedfb39673e9225e757400b5903eeaed9ff
masozow <14838605+masozow@users.noreply.github.com>
2025-05-01 09:48:32 -0600

    changing the way the component looks

--- src/app/dashboard/input-otp/page.tsx
+++ src/app/dashboard/input-otp/page.tsx
@@ -12,14 +12,22 @@ const Page = () => {
     <div className="flex flex-col justify-center items-center h-[250px]">
       {" "}
       <InputOTP
-        maxLength={4}
+        maxLength={6}
         value={value}
         onChange={(value) => setValue(value)}>
         <InputOTPGroup>
           <InputOTPSlot index={0} />
           <InputOTPSlot index={1} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
           <InputOTPSlot index={2} />
           <InputOTPSlot index={3} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
         </InputOTPGroup>
       </InputOTP>
       <div className="mt-4">{value}</div>
